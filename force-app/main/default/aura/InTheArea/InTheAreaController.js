079fd0bbaab893516789e18eb392d6dcda417035
John Meinen <john@meinen.info>
2017-08-26 20:17:51 +0200

    Add design parameters for App Builder

--- force-app/main/default/aura/InTheArea/InTheAreaController.js
+++ force-app/main/default/aura/InTheArea/InTheAreaController.js
@@ -1,5 +1,8 @@
 ({
     doInit: function(component, event, helper) {
+        var main = component.find('main');
+        $A.util.removeClass(main, 'small');
+        $A.util.addClass(main, component.get("v.designHeight"));        
         helper.getLocalList(component);
     },
     updateSearch: function(component, event, helper) {
